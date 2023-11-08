7f24a94cf2123da71afcc02979d687725d5fb160
Artem Samokhin <artem.samokhin.2001@mail.ru>
2023-11-08 11:15:06 +0300

    create show full items

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} alt={this.props.item.description} />
+        <img src={"./img/" + this.props.item.img} alt={this.props.item.description} onClick={() => this.props.onShowItem(this.props.item)} />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.description}</p>
         <b>{this.props.item.price} &#8381;</b>
